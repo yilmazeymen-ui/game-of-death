f26226f17a4a15dd938928362bd86cb00b6bc16c
yilmazeymen-ui <yilmazeymen986@gmail.com>
2026-06-21 18:52:38 +0300

    First functioning window

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,27 @@
 
 int main(int argc, char *argv[])
 {
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
         printf("error initializing SDL: %s\n", SDL_GetError());
         return 1;
     }
-    SDL_Window* win = SDL_CreateWindow("GAME", 1000, 1000, 0);
 
-    while (1)
-        ;
+    SDL_Window* win = SDL_CreateWindow("GAME", 500, 500, 0);
+    if (win == nullptr) {
+        printf("\nError window: %s", SDL_GetError());
+    }
 
+    SDL_Renderer* render = SDL_CreateRenderer(win, nullptr);
+    if (render == nullptr) {
+        printf("\nError render: %s", SDL_GetError());
+    }
+
+    SDL_SetRenderDrawColor(render, 0, 0, 120, 255);
+    SDL_RenderClear(render);
+    SDL_RenderPresent(render);
+
+    SDL_Delay(3000);
+    SDL_DestroyRenderer(render);
+    SDL_Quit();
     return 0;
 }
